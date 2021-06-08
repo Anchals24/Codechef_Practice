2b9a555e7928c47716a9d79fead2751a281edb96
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-06-08 14:52:44 +0530

    Update May long challenge codechef.py

--- Codechef Contests/MAY LONG CONTEST/May long challenge codechef.py
+++ Codechef Contests/MAY LONG CONTEST/May long challenge codechef.py
@@ -11,3 +11,27 @@ for t in range(T):
         PT = (100 - X) * B
         Ans = (PT + A) * 10
     print(Ans)
+    
+    
+#Question : Golf
+
+#First Approach : Got TLE...
+
+
+#Optimized Approach
+
+T = int(input())
+for t in range(T):
+    Inp = input()
+    L = Inp.split()
+    N = int(L[0])
+    x = int(L[1])
+    k = int(L[2])
+    d1 = x - 0
+    d2 = (N+1) - x
+    if d1 % k == 0:
+        print("Yes")
+    elif d2 % k == 0:
+        print("Yes")
+    else:
+        print("No")
