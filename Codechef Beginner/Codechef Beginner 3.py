4a476fc3bb66593e9ac658b1959763c42d8a81b6
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-06-17 09:10:33 +0530

    Codechef beginner level programs.

---
+++ Codechef Beginner/Codechef Beginner 3.py
@@ -0,0 +1,20 @@
+""" 
+Codechef Beginner :
+
+Problem : Add Two Numbers 
+
+"""
+
+#Solution: 
+
+Num = input()
+T = int(Num)
+for t in range(T):
+    x = input()
+    first_s = x.split()
+    A = int(first_s[0])
+    B = int(first_s[1])
+    C = A + B
+    print(C)
+
+#Complexity : O(n)
