6ff92d8401735f3b2a1ac7f5918b77693224e430
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-06-18 09:11:24 +0530

    Codechef Beginner Level Programs.

---
+++ Codechef Beginner/Codechef Beginner 4.py
@@ -0,0 +1,12 @@
+""" 
+Codechef Beginner :
+
+Problem : Number Mirror 
+
+"""
+#Solution : It was the easiest question... we just had to print whatever was the given input!
+
+n = input()
+print(n)
+
+#Complexity : O(1)
