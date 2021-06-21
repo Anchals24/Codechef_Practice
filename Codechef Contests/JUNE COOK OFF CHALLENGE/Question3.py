851a98bac3b21de23e2e0e3ecba88c7bc2b5bdc0
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-06-21 09:20:34 +0530

    Codechef Contests.

--- Codechef Contests/JUNE COOK OFF CHALLENGE/Question3.py
+++ Codechef Contests/JUNE COOK OFF CHALLENGE/Question3.py
@@ -24,3 +24,34 @@ for q in range(Q):
 
 #Time Complexity: O(q*r)
 
+#Solution_2 : Optimized Approach..
+
+from bisect import bisect_left
+
+t1 = input()
+n = int(t1.split()[0])
+q = int(t1.split()[1])
+t2 = input()
+a = []
+for n in t2.split():
+    a.append(int(n))
+a.sort()
+l = len(a)
+for i in range(q):
+    n = int(input())
+    idx = bisect_left(a, n)
+    if idx < l and a[idx] == n:
+        print(0)
+    elif (l-idx == 0 ):
+        if l%2 == 0:
+            print('POSITIVE')
+        else:
+            print('NEGATIVE')
+    elif (l-idx)%2 == 0:
+        print('POSITIVE')
+    else:
+        print('NEGATIVE')
+        
+# cook your dish here
+
+
