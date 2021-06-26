03e7523dec8b297c7eef2901d55c45f9ae8352cb
Anchal_Sharma <71165891+Anchals24@users.noreply.github.com>
2021-06-26 11:33:35 +0530

    Codechef Beginner Level Programs.

---
+++ Codechef Beginner/Codechef Beginner 14.py
@@ -0,0 +1,12 @@
+""" 
+Codechef Beginner :
+Problem :  Helping Chef.
+"""
+
+T = int(input())
+for t in range(T):
+    N = int(input())
+    if N < 10:
+        print('Thanks for helping Chef!')
+    else:
+        print('-1')
